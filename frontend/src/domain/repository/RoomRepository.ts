7341c896255d5264e3fef3a9eaa03661a1abd931
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-05 23:47:13 +0700

    interface roomrepo

--- frontend/src/domain/repository/RoomRepository.ts
+++ frontend/src/domain/repository/RoomRepository.ts
@@ -2,4 +2,8 @@ import {Room} from "../model/Room.ts";
 
 export interface RoomRepository {
     getRooms(): Promise<Room[]>;
+    getRoomsActive(roomNumberPrefix?: string): Promise<Room[]>;
+    getRoomsInactive(roomNumberPrefix?: string): Promise<Room[]>;
+    createRoom(roomNumber: string): Promise<Room>;
+    removeRoom(roomNumber: string): Promise<Room>;
 }
