a5b35f1ba227167a39844a7ae37f8b5edb621cdb
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-05 13:22:31 +0700

    fix item mapping

--- frontend/src/data/dataSource/API/RoomTransactionAPIDatasourceImpl.ts
+++ frontend/src/data/dataSource/API/RoomTransactionAPIDatasourceImpl.ts
@@ -9,16 +9,16 @@ export default class RoomTransactionAPIDatasourceImpl implements RoomTransaction
     async getRoomTransactions(): Promise<RoomTransaction[]> {
         try {
             const response = await axios.get(BASE_URL);
-            return response.data.map((item: RoomTransactionAPIEntity) => {
-                item.id
-                item.borrowerUsername
-                item.borrowerDivision
-                item.returnerUsername
-                item.returnerDivision
-                item.roomNumber
-                item.roomIn
-                item.roomOut
-            })
+            return response.data.map((item: RoomTransactionAPIEntity) => ({
+                id: item.id,
+                BorrowerUsername: item.borrowerUsername,
+                BorrowerDivision: item.borrowerDivision,
+                ReturnerUsername: item.returnerUsername,
+                ReturnerDivision: item.returnerDivision,
+                RoomNumber: item.roomNumber,
+                RoomIn: new Date(item.roomIn),
+                RoomOut: new Date(item.roomOut)
+            }))
         } catch (e) {
             console.log(e);
             throw(e);
