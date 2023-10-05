561876c16cf3ee962a5c35e17959612aeb2506b9
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-05 14:25:31 +0700

    add 4 more request

--- frontend/src/data/dataSource/RoomDatasource.ts
+++ frontend/src/data/dataSource/RoomDatasource.ts
@@ -2,4 +2,8 @@ import {Room} from "../../domain/model/Room.ts";
 
 export default interface RoomDatasource {
     getRooms(): Promise<Room[]>;
+    getRoomsActive(roomNumberPrefix?: string): Promise<Room[]>;
+    getRoomsInactive(roomNumberPrefix?: string): Promise<Room[]>;
+    createRoom(roomNumber: string): Promise<Room>;
+    removeRoom(roomNumber: string): Promise<Room>;
 }
