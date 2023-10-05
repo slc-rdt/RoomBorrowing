9650a9aba8173b0d580496e9df12fa0f1c201627
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-06 00:32:15 +0700

    RoomTransactionAPIImpl done

--- frontend/src/data/dataSource/API/RoomTransactionAPIDatasourceImpl.ts
+++ frontend/src/data/dataSource/API/RoomTransactionAPIDatasourceImpl.ts
@@ -6,19 +6,48 @@ import {RoomTransactionAPIEntity} from "./Entity/RoomTransactionAPIEntity.ts";
 const BASE_URL = "http://localhost:8080/api/room-transactions/";
 
 export default class RoomTransactionAPIDatasourceImpl implements RoomTransactionDatasource {
+    private axiosInstance = axios.create({
+        baseURL: BASE_URL,
+        transformResponse: [function (response) {
+            let resp
+
+            try {
+                resp = JSON.parse(response)
+            } catch (error) {
+                throw Error(`[requestClient] Error parsing response JSON data - ${JSON.stringify(error)}`)
+            }
+
+            if (resp.status === 'OK') {
+                if (resp.data && resp.data.data) {
+                    const transformedResponse = resp.data.data.map((item: RoomTransactionAPIEntity): RoomTransaction => ({
+                        id: item.id,
+                        borrowerUsername: item.borrowerUsername,
+                        borrowerDivision: item.borrowerDivision,
+                        returnerUsername: item.returnerUsername,
+                        returnerDivision: item.returnerDivision,
+                        roomNumber: item.roomNumber,
+                        roomIn: new Date(item.roomIn),
+                        roomOut: new Date(item.roomOut)
+                    }));
+                    console.log(response);
+                    return {
+                        ...response,
+                        data: transformedResponse
+                    };
+                }
+                return resp.data
+            } else {
+                throw Error(`[requestClient] Request failed with reason -  ${response}`)
+            }
+        }],
+    })
+
     async getRoomTransactions(): Promise<RoomTransaction[]> {
         try {
-            const response = await axios.get(BASE_URL);
-            return response.data.map((item: RoomTransactionAPIEntity): RoomTransaction => ({
-                id: item.id,
-                borrowerUsername: item.borrowerUsername,
-                borrowerDivision: item.borrowerDivision,
-                returnerUsername: item.returnerUsername,
-                returnerDivision: item.returnerDivision,
-                roomNumber: item.roomNumber,
-                roomIn: new Date(item.roomIn),
-                roomOut: new Date(item.roomOut)
-            }))
+            return await this.axiosInstance({
+                method: "get",
+                url: "/"
+            });
         } catch (e) {
             console.log(e);
             throw(e);
