4598528e0b105a51b342ae30fdb8cfd87a2427cf
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-05 12:54:01 +0700

    getRoomTransactions done

--- frontend/src/data/dataSource/API/RoomTransactionAPIDatasourceImpl.ts
+++ frontend/src/data/dataSource/API/RoomTransactionAPIDatasourceImpl.ts
@@ -1,11 +1,28 @@
 import RoomTransactionDatasource from "../RoomTransactionDatasource.ts";
 import {RoomTransaction} from "../../../domain/model/RoomTransaction.ts";
+import axios from "axios";
+import {RoomTransactionAPIEntity} from "./Entity/RoomTransactionAPIEntity.ts";
 
 const BASE_URL = "http://localhost:8080/api/room-transactions/";
 
 export default class RoomTransactionAPIDatasourceImpl implements RoomTransactionDatasource {
     async getRoomTransactions(): Promise<RoomTransaction[]> {
-        return Promise.resolve([]);
+        try {
+            const response = await axios.get(BASE_URL);
+            return response.data.map((item: RoomTransactionAPIEntity) => {
+                item.id
+                item.borrowerUsername
+                item.borrowerDivision
+                item.returnerUsername
+                item.returnerDivision
+                item.roomNumber
+                item.roomIn
+                item.roomOut
+            })
+        } catch (e) {
+            console.log(e);
+            throw(e);
+        }
     }
 
 }
