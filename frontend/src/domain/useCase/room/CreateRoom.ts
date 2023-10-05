bda4d3f0db6c8ca80161cf29a3d44b5e65ddeb01
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-06 00:10:12 +0700

    Create CreateRoom.ts

---
+++ frontend/src/domain/useCase/room/CreateRoom.ts
@@ -0,0 +1,19 @@
+import {Room} from "../../model/Room.ts";
+import {RoomRepository} from "../../repository/RoomRepository.ts";
+
+export interface CreateRoomUseCase {
+    invoke: (val: string) => Promise<Room>;
+}
+
+export class CreateRoom implements CreateRoomUseCase {
+    private roomRepo: RoomRepository;
+
+    constructor(_roomrepo: RoomRepository) {
+        this.roomRepo = _roomrepo;
+    }
+
+    invoke(val: string): Promise<Room> {
+        return this.roomRepo.createRoom(val);
+    }
+
+}
