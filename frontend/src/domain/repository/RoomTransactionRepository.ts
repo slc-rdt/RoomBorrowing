b09f97893d3cd2d675732526a60bdbac95568e7e
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-06 12:16:48 +0700

    borrow and return repo

--- frontend/src/domain/repository/RoomTransactionRepository.ts
+++ frontend/src/domain/repository/RoomTransactionRepository.ts
@@ -1,6 +1,10 @@
 import {RoomTransaction} from "../model/RoomTransaction.ts";
+import {RoomTransactionBorrowAPIRequest} from "../../data/dataSource/API/Request/RoomTransactionBorrowAPIRequest.ts";
+import {RoomTransactionReturnAPIRequest} from "../../data/dataSource/API/Request/RoomTransactionReturnAPIRequest.ts";
 
 export interface RoomTransactionRepository {
     getRoomTransactions(): Promise<RoomTransaction[]>;
     getRoomTransactionsActive(roomNumber?: string): Promise<RoomTransaction[]>;
+    borrowRoom(data: RoomTransactionBorrowAPIRequest): Promise<RoomTransaction>;
+    returnRoom(data: RoomTransactionReturnAPIRequest): Promise<RoomTransaction>;
 }
