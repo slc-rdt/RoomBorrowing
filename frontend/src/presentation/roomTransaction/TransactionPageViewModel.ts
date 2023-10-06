c7ae6a0d1171a99afe7fda4533c9f6dab0a358ea
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-06 14:11:10 +0700

    transactpage view model

--- frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
+++ frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
@@ -1,6 +1,23 @@
+import {useState} from "react";
+import {Room} from "../../domain/model/Room.ts";
+import RoomAPIDatasourceImpl from "../../data/dataSource/API/RoomAPIDatasourceImpl.ts";
+import {RoomRepositoryImpl} from "../../data/repository/RoomRepositoryImpl.ts";
+import {GetRooms} from "../../domain/useCase/room/GetRooms.ts";
+
 export function TransactionPageViewModel() {
-    // const roomDataSourceImpl = new RoomAPIDatasourceImpl();
-    // const roomsRepositoryImpl = new RoomRepositoryImpl(roomDataSourceImpl);
-    // const getRoomsUseCase = new GetRooms(roomsRepositoryImpl);
-    // console.log(getRoomsUseCase.invoke().then((resp) => console.log(resp)))
+    const [rooms, setRooms] = useState<Room[]>([]);
+
+    const roomsDataSourceImpl = new RoomAPIDatasourceImpl();
+    const roomsRepositoryImpl = new RoomRepositoryImpl(roomsDataSourceImpl);
+
+    const getRoomsUseCase = new GetRooms(roomsRepositoryImpl);
+
+    async function getRooms() {
+        setRooms(await getRoomsUseCase.invoke());
+    }
+
+    return {
+        rooms,
+        getRooms
+    }
 }
