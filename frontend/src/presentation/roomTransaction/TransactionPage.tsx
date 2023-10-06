856cfc5b01a3a95527206f8bd744911656a5a810
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-06 13:47:57 +0700

    revert to original

--- frontend/src/presentation/roomTransaction/TransactionPage.tsx
+++ frontend/src/presentation/roomTransaction/TransactionPage.tsx
@@ -1,15 +1,44 @@
-import React, {FC} from "react";
 import ParticleBG from "../../pages/homePage/ParticleBG.tsx";
 import {Select} from "@chakra-ui/react";
 import {Stack} from "@chakra-ui/layout";
 import {Input} from "@chakra-ui/input";
-import {ModalPlaceholder} from "./TransactionPageViewModel.ts";
+import React, {FC, useEffect, useState} from "react";
+
+interface ModalPlaceholder {
+    username: string,
+    division: string,
+    roomNumber: string,
+}
+
+const borrowPlaceholder: ModalPlaceholder = {
+    username: "Borrower's Username",
+    division: "Borrower's Division",
+    roomNumber: "Room Number",
+}
+
+const returnerPlaceholder: ModalPlaceholder = {
+    username: "Returner's Username",
+    division: "Returner's Division",
+    roomNumber: "Room Number",
+}
 
 interface TransactionPageProps {
-    placeholder: ModalPlaceholder;
-    handleSelectChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+
 }
-const TransactionPage: FC<TransactionPageProps> = ({placeholder, handleSelectChange}) => {
+const TransactionPage: FC<TransactionPageProps> = ({}) => {
+    const [borrow, setBorrow] = useState<boolean>(true)
+    const [placeholder, setPlaceholder] = useState<ModalPlaceholder>(borrowPlaceholder)
+
+    useEffect(() => {
+        if (borrow) setPlaceholder(borrowPlaceholder)
+        else setPlaceholder(returnerPlaceholder)
+
+    }, [borrow])
+
+    function handleSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
+        const val = e.currentTarget.value;
+        setBorrow(val === 'borrow');
+    }
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center gradient'>
             <ParticleBG />
--- frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
+++ frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
@@ -1,48 +1,6 @@
-import React, {useEffect, useState} from "react";
-// import RoomAPIDatasourceImpl from "../../data/dataSource/API/RoomAPIDatasourceImpl.ts";
-// import {RoomRepositoryImpl} from "../../data/repository/RoomRepositoryImpl.ts";
-// import {GetRooms} from "../../domain/useCase/room/GetRooms.ts";
-
-export interface ModalPlaceholder {
-    username: string,
-    division: string,
-    roomNumber: string,
-}
-
-const borrowPlaceholder: ModalPlaceholder = {
-    username: "Borrower's Username",
-    division: "Borrower's Division",
-    roomNumber: "Room Number",
-}
-
-const returnerPlaceholder: ModalPlaceholder = {
-    username: "Returner's Username",
-    division: "Returner's Division",
-    roomNumber: "Room Number",
-}
-
 export function TransactionPageViewModel() {
-    const [borrow, setBorrow] = useState<boolean>(true)
-    const [placeholder, setPlaceholder] = useState<ModalPlaceholder>(borrowPlaceholder)
-
     // const roomDataSourceImpl = new RoomAPIDatasourceImpl();
     // const roomsRepositoryImpl = new RoomRepositoryImpl(roomDataSourceImpl);
     // const getRoomsUseCase = new GetRooms(roomsRepositoryImpl);
     // console.log(getRoomsUseCase.invoke().then((resp) => console.log(resp)))
-
-    useEffect(() => {
-        if (borrow) setPlaceholder(borrowPlaceholder)
-        else setPlaceholder(returnerPlaceholder)
-
-    }, [borrow])
-
-    function handleSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
-        const val = e.currentTarget.value;
-        setBorrow(val === 'borrow');
-    }
-
-    return {
-        placeholder,
-        handleSelectChange
-    }
 }
