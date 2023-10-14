17b237cca6ba395cc3e1862b04490a9ad881a580
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-14 10:43:34 +0700

    working opts

--- frontend/src/presentation/roomTransaction/TransactionPage.tsx
+++ frontend/src/presentation/roomTransaction/TransactionPage.tsx
@@ -1,9 +1,14 @@
 import ParticleBG from "./ParticleBG.tsx";
-import {Select} from "@chakra-ui/react";
+import {Button} from "@chakra-ui/react";
+// import {Button, Select} from "@chakra-ui/react";
 import {Stack} from "@chakra-ui/layout";
 import {Input} from "@chakra-ui/input";
+import Select from 'react-select';
 import React, {FC, useEffect, useState} from "react";
+import useViewModel from "./TransactionPageViewModel.ts";
 import './TransactionPage.css'
+import AsyncSelect from "react-select/async";
+import {Room} from "../../domain/model/Room.ts";
 
 interface ModalPlaceholder {
     username: string,
@@ -27,8 +32,21 @@ interface TransactionPageProps {
 
 }
 const TransactionPage: FC<TransactionPageProps> = () => {
+    const {
+        opts,
+        getRooms,
+    } = useViewModel()
     const [borrow, setBorrow] = useState<boolean>(true)
     const [placeholder, setPlaceholder] = useState<ModalPlaceholder>(borrowPlaceholder)
+
+    useEffect(() => {
+        getRooms()
+
+    }, [])
+
+    useEffect(() => {
+        console.log(opts);
+    }, [opts])
 
     useEffect(() => {
         if (borrow) setPlaceholder(borrowPlaceholder)
@@ -40,19 +58,54 @@ const TransactionPage: FC<TransactionPageProps> = () => {
         const val = e.currentTarget.value;
         setBorrow(val === 'borrow');
     }
+
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center gradient'>
             <ParticleBG />
             <div className='w-[30%] h-[50%]'>
-                <Select size='lg' bg={'white'} onChange={handleSelectChange}>
-                    <option value='borrow'>Borrow</option>
-                    <option value='return'>Return</option>
-                </Select>
-                <div className='h-4'></div>
-                <Stack spacing={3} className='w-full h-full bg-white rounded-lg px-8 py-10'>
-                    <Input placeholder={placeholder.username} size='lg' />
-                    <Input placeholder={placeholder.division} size='lg' />
-                </Stack>
+                {/*<Select size='lg' bg={'white'} onChange={handleSelectChange}>*/}
+                {/*    <option value='borrow'>Borrow</option>*/}
+                {/*    <option value='return'>Return</option>*/}
+                {/*</Select>*/}
+                {/*<Select*/}
+                {/*    name="room"*/}
+                {/*    className="basic-single"*/}
+                {/*    classNamePrefix="select"*/}
+                {/*    isClearable={true}*/}
+                {/*    isSearchable={true}*/}
+                {/*    options={rooms}*/}
+                {/*/>*/}
+                {
+                    !opts ?
+                        <div className='w-full h-full bg-white rounded-lg px-8 py-10'>
+                            Loading...
+                        </div>
+                        :
+                        <div>
+                            {/*<AsyncSelect*/}
+                            {/*    options={opts}*/}
+                            {/*    cacheOptions*/}
+                            {/*    loadOptions={getRoomOptions}*/}
+                            {/*    defaultOptions={opts}*/}
+                            {/*/>*/}
+                            <Select
+                                name="room"
+                                className="basic-single"
+                                classNamePrefix="select"
+                                isClearable={true}
+                                isSearchable={true}
+                                options={opts}
+                            />
+                            <div className='h-4'></div>
+                            <Stack spacing={3} className='w-full h-full bg-white rounded-lg px-8 py-10'>
+                                <Input placeholder={placeholder.username} size='lg' />
+                                <Input placeholder={placeholder.division} size='lg' />
+                                <Button colorScheme='gray' size='lg'>Transaction</Button>
+                                <Button colorScheme='green' size='lg'>Borrow</Button>
+                                <Button colorScheme='blue' size='lg'>Return</Button>
+                            </Stack>
+                        </div>
+                }
             </div>
         </div>
     );
--- frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
+++ frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
@@ -1,10 +1,11 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Room} from "../../domain/model/Room.ts";
 import RoomAPIDatasourceImpl from "../../data/dataSource/API/RoomAPIDatasourceImpl.ts";
 import {RoomRepositoryImpl} from "../../data/repository/RoomRepositoryImpl.ts";
 import {GetRooms} from "../../domain/useCase/room/GetRooms.ts";
 
 export default function TransactionPageViewModel() {
+    const [opts, setOpts] = useState<{ value: string; label: string; }[]>([])
     const [rooms, setRooms] = useState<Room[]>([]);
     const [room, setRoom] = useState<Room>();
 
@@ -14,13 +15,22 @@ export default function TransactionPageViewModel() {
     const getRoomsUseCase = new GetRooms(roomsRepositoryImpl);
 
     async function getRooms(roomNumberPrefix?: string) {
-        setRooms(await getRoomsUseCase.invoke(roomNumberPrefix));
+        const res = await getRoomsUseCase.invoke(roomNumberPrefix);
+        setRooms(res);
     }
 
+    useEffect(()=>{
+        setOpts(rooms.map(room => ({
+            value: room.number,
+            label: `Room ${room.number}`
+        })));
+    }, [rooms])
+
     return {
+        opts,
         rooms,
         room,
         setRoom,
-        getRooms
+        getRooms,
     }
 }
