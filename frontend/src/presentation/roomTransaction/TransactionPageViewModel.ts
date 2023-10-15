cddbd6f02b5c7c75b7cf0d725cd8d953de671880
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-15 14:34:49 +0700

    cleanup

--- frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
+++ frontend/src/presentation/roomTransaction/TransactionPageViewModel.ts
@@ -92,8 +92,6 @@ export default function TransactionPageViewModel() {
         } else {
             setDisabled(false)
         }
-
-        console.log(rooms);
     }, [rooms])
 
     useEffect(() => {
