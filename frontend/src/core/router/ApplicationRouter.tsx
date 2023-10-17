63c8e998d10a4fe2274be9ceceab1e4e1f87aef1
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-17 10:54:55 +0700

    HistoryRoute

--- frontend/src/core/router/ApplicationRouter.tsx
+++ frontend/src/core/router/ApplicationRouter.tsx
@@ -3,11 +3,16 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import TransactionPage from "../../presentation/roomTransaction/TransactionPage.tsx";
+import {HistoryPage} from "../../presentation/history/HistoryPage.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <TransactionPage />,
+    },
+    {
+        path: "/history",
+        element: <HistoryPage />,
     },
 ]);
 
