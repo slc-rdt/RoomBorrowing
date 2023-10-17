fb68d63ad85cbf7bebcb4cc2a03d7ccdf7ffa69e
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-17 12:03:50 +0700

    history page done

--- frontend/src/presentation/history/HistoryPage.tsx
+++ frontend/src/presentation/history/HistoryPage.tsx
@@ -1,9 +1,55 @@
 import {GradientBG} from "../gradientBackground/GradientBG.tsx";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
+import useViewModel from "./HistoryPageViewModel.ts";
 
 export const HistoryPage = () => {
+    const {
+        roomTransactions
+    } = useViewModel()
+
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center relative'>
             <GradientBG />
+
+            <TableContainer className='z-10 bg-white rounded-lg shadow-2xl'>
+                <Table variant='striped' colorScheme='teal'>
+                    <TableCaption>Imperial to metric conversion factors</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>Room Number</Th>
+                            <Th>Borrower</Th>
+                            <Th>Borrower Division</Th>
+                            <Th>Returner</Th>
+                            <Th>Returner Division</Th>
+                            <Th>Borrow Date</Th>
+                            <Th>Return Date</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {
+                            roomTransactions?.map(x => <Tr>
+                                <Td>{x.roomNumber}</Td>
+                                <Td>{x.borrowerUsername}</Td>
+                                <Td>{x.borrowerDivision}</Td>
+                                <Td>{x.returnerUsername}</Td>
+                                <Td>{x.returnerDivision}</Td>
+                                <Td>{x.roomIn.toLocaleString()}</Td>
+                                <Td>{x.roomOut.toLocaleString()}</Td>
+                            </Tr>)
+                        }
+                    </Tbody>
+                </Table>
+            </TableContainer>
         </div>
     );
 };
