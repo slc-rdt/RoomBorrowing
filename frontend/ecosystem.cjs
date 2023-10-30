a255fcfbb3110384aed0a256587dba86c02fe367
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-10-30 13:14:16 +0700

    chore: final config for deploy

--- frontend/ecosystem.cjs
+++ frontend/ecosystem.cjs
@@ -1,8 +1,9 @@
 module.exports = {
     script: "serve",
+    name: 'roomBorrowing-frontend',
     env: {
         PM2_SERVE_PATH: 'build',
-        PM2_SERVE_PORT: 8080,
+        PM2_SERVE_PORT: 6971,
         PM2_SERVE_SPA: 'true',
         PM2_SERVE_HOMEPAGE: '/index.html'
     }
