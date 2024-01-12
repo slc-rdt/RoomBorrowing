3ac2bcad1b0f136ff6d79ddad3603f05ec9fe5cc
Arya <64455337+H-A-L-F@users.noreply.github.com>
2024-01-12 11:18:15 +0700

    feat: added history button to view history

--- frontend/src/presentation/home/HomePage.tsx
+++ frontend/src/presentation/home/HomePage.tsx
@@ -3,6 +3,8 @@ import ParticleBG from "../roomTransaction/ParticleBG.tsx";
 import useViewModel from "./HomePageViewModel.ts";
 import {TransactionForm} from "../transactionForm/TransactionForm.tsx";
 import {TransactionTypeBorrow, TransactionTypeReturn} from "../../core/lib/Lib.ts";
+import {Link} from "react-router-dom";
+import {Button} from "@chakra-ui/react";
 
 export const HomePage = () => {
     const {
@@ -12,9 +14,10 @@ export const HomePage = () => {
     } = useViewModel();
 
     return (
-        <div className='w-screen h-screen flex flex-col justify-center items-center'>
+        <div className='w-screen h-screen flex flex-col justify-center items-center relative'>
             <GradientBG />
             <ParticleBG />
+            <Link className='!absolute bottom-8' to={"/history"}><Button className='!text-xl !px-12 !py-6' colorScheme='blue'>History</Button></Link>
             <div className='flex flex-row w-full h-full'>
                 <div className='flex flex-col w-1/2 justify-center items-center'>
                     <TransactionForm transactionType={TransactionTypeBorrow} opts={borrowOpts} transactionCallback={transactionCallback}/>
@@ -23,6 +26,7 @@ export const HomePage = () => {
                     <TransactionForm transactionType={TransactionTypeReturn} opts={returnOpts} transactionCallback={transactionCallback}/>
                 </div>
             </div>
+
         </div>
     );
 };
